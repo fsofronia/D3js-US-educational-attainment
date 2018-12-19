68f0b7c3d8b95c59ec1c30b092de77aa5e23ea21
Sofronia Foutsitzi P2017063 <44113197+fsofronia@users.noreply.github.com>
2018-12-19 18:02:45 +0200

    Update script_1.js

--- assets/scripts/script_1.js
+++ assets/scripts/script_1.js
@@ -1,5 +1,9 @@
   // Script 1
 // Data Visualization III - Pie Chart
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
 var donut = donutChart()
         .width(960)
         .height(450)
@@ -7,6 +11,7 @@ var donut = donutChart()
         .padAngle(0.2) // effectively dictates the gap between slice
         .variable('Percent')
         .category('Education Level');
+
 
     d3.tsv('assets/data/species.tsv', function(error, data) {
         if (error) throw error;
